609d31f19439329e8084bf83ec0a6013fb51d05f
nit <contact@2bam.com>
2018-01-26 22:00:17 -0300

    Initial commit

---
+++ Modem/Assets/Scripts/Test.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class Test : MonoBehaviour {
+
+	// Use this for initialization
+	IEnumerator Start () {
+		var aud = GetComponent<AudioSource>();
+		aud.clip = Microphone.Start("Built-in Microphone", true, 10, 44100);
+		aud.loop = true;
+		while (!(Microphone.GetPosition(null) > 0)) {
+			yield return null;
+			Debug.Log("Waiting mic");
+		}
+		aud.Play();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		var c = GetComponent<AudioMeasureCS>();
+		GetComponent<Text>().text = "P" + c.PitchValue + " V" + c.DbValue;
+	
+	}
+}
