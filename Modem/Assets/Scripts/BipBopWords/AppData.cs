0301fbd2ae1d45fa3d0561646568a25ab32c7270
Lorena Weder <latamakuchi@gmail.com>
2018-01-27 15:50:26 -0300

    Using enum, renamed to Word and made words into a singleton.

---
+++ Modem/Assets/Scripts/BipBopWords/AppData.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+
+public class AppData : Singleton<AppData>
+{
+	[SerializeField] private TextAsset _wordsDefinition;
+	public List<Word> AvailableWords { get; private set; }
+
+	void Awake()
+	{
+		List<string> words = new List<string>();
+
+		using (var reader = new StringReader(_wordsDefinition.text))
+		{
+			string line;
+			while ((line = reader.ReadLine()) != null)
+			{
+				if(line.StartsWith("-- "))
+				{
+					var newline = line.Substring(3);
+					// Add to different categories until finding a new category.
+				}
+				else words.Add(line);
+			}
+		}
+
+		AvailableWords = new List<Word>(words.Count);
+		for (var i = 0; i < words.Count; i++)
+		{
+			AvailableWords.Add(new Word(words[i], i));
+		}
+	}
+}
