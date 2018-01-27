6afd05e00ae7633bc0f8aedbce68b057f0c42efc
Lorena Weder <latamakuchi@gmail.com>
2018-01-27 00:44:41 -0300

    Added initial words to beeps test.

---
+++ Modem/Assets/Scripts/BipBopWords/BipBopWord.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Assertions;
+
+public enum SoundChars
+{
+	Bip = 0,
+	Bop = 1,
+	Grr = 2,
+	Gni = 3
+}
+
+public class BipBopWord
+{
+	public const int MAX_DIGITS = 3;
+
+	public string Word { get; private set; }
+	public int Id { get; private set; }
+	public int[] BipBopValues { get; private set; }
+
+	public BipBopWord(string word, int id)
+	{
+		Word = word;
+		Id = id;
+
+		if (BipBopValues == null) BipBopValues = new int[MAX_DIGITS];
+		IntToBipBop(Id, 4, MAX_DIGITS, BipBopValues);
+	}
+
+	public static void IntToBipBop(int @decimal, int @base, int maxDigits, int[] values)
+	{
+		if (@decimal > Mathf.Pow(@base, maxDigits))
+		{
+			Debug.LogError("Value would require more digits than allowed.");
+			return;
+		}
+
+		// Clear to zeroes.
+		for (var i = 0; i < values.Length; i++) values[i] = 0;
+
+		if (@decimal <= 0) return;
+
+		// Fill from right to left.
+		var currentDigit = maxDigits - 1;
+		while (@decimal > 0)
+		{
+			int digit = @decimal % @base;
+			values[currentDigit]= digit;
+			@decimal = @decimal / @base;
+			currentDigit--;
+		}
+	}
+}
---
+++ Modem/Assets/Scripts/BipBopWords/WordsGenerator.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+
+public class WordsGenerator : MonoBehaviour
+{
+	public int debugIndex;
+
+	[SerializeField] private TextAsset _wordsDefinition;
+	private List<BipBopWord> _availableWords;
+
+	void Awake()
+	{
+		List<string> words = new List<string>();
+
+		using (var reader = new StringReader(_wordsDefinition.text))
+		{
+			string line;
+			while ((line = reader.ReadLine()) != null)
+			{
+				words.Add(line);
+			}
+		}
+
+		_availableWords = new List<BipBopWord>(words.Count);
+		for (var i = 0; i < words.Count; i++)
+		{
+			_availableWords.Add(new BipBopWord(words[i], i));
+		}
+	}
+
+	private void Update()
+	{
+		if (Input.GetKeyDown(KeyCode.B))
+		{
+			if (debugIndex > _availableWords.Count) return;
+			print("<color=white>------------------------------------</color>");
+			print("Int: " + debugIndex + " Word: " + _availableWords[debugIndex].Word);
+			foreach(var b in _availableWords[debugIndex].BipBopValues)
+			{
+				print(((SoundChars)b).ToString());
+			}
+		}
+	}
+}
